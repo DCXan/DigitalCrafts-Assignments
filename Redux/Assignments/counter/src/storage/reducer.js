b41f6f39f74f6fb0f5ee042c50e9aabc2cece024
DCXan <chen.david.1016@gmail.com>
2022-07-18 13:40:57 -0500

    converted reducer to switch statements

--- Redux/Assignments/counter/src/storage/reducer.js
+++ Redux/Assignments/counter/src/storage/reducer.js
@@ -4,26 +4,51 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
-    if (action.type == 'DECREMENT') {
-        return {
-            ...state,
-            counter: state.counter - 1
-        }
-    } else if (action.type == 'INCREMENT') {
-        return {
-            ...state,
-            counter: state.counter + 1
-        }
-    } else if (action.type == 'SUBTRACT') {
-        return {
-            ...state,
-            counter: state.counter - action.value
-        }
-    } else if (action.type == 'ADD') {
-        return {
-            ...state,
-            counter: state.counter + action.value
-        }
+    // if (action.type == 'DECREMENT') {
+    //     return {
+    //         ...state,
+    //         counter: state.counter - 1
+    //     }
+    // } else if (action.type == 'INCREMENT') {
+    //     return {
+    //         ...state,
+    //         counter: state.counter + 1
+    //     }
+    // } else if (action.type == 'SUBTRACT') {
+    //     return {
+    //         ...state,
+    //         counter: state.counter - action.value
+    //     }
+    // } else if (action.type == 'ADD') {
+    //     return {
+    //         ...state,
+    //         counter: state.counter + action.value
+    //     }
+    // }
+
+    switch (action.type) {
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        default:
+            break
     }
 
     return state
